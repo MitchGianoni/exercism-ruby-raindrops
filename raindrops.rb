8ce346e0da35c0f90b10d51bcb9de25ff6888aff
MitchGianoni <mitchgianoni@yahoo.com>
2018-05-25 00:19:17 -0400

    added raindrops.rb and passed 2 tests

---
+++ raindrops.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+class Raindrops
+  def self.convert(num)
+    answer = ''
+
+    #binding.pry
+
+    if num % 3 == 0
+      answer += 'Pling'
+    elsif num = 1
+      return "#{num}"
+    end
+
+    return answer
+
+  end
+end
+
+
+# - If the number has 3 as a factor, output 'Pling'.
+# - If the number has 5 as a factor, output 'Plang'.
+# - If the number has 7 as a factor, output 'Plong'.
+# - If the number does not have 3, 5, or 7 as a factor,
+#   just pass the number's digits straight through.
